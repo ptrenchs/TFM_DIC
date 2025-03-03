81655738af7b559571c240ccbd1d76562629f480
Thrinky_asus <ptrenchs@gmail.com>
2025-03-03 12:33:17 +0100

    Actualizacion del documento "clonar_repositorio.sh" para google colab 4

--- clonar_repositorio.sh
+++ clonar_repositorio.sh
@@ -18,6 +18,9 @@ else
         echo "Archivo requirements.txt encontrado en: $REQ_FILE_0"
         echo "Instalando dependencias..."
         pip install -r "$REQ_FILE_0"
+        pip install dill
+        pip install noise
+
     else
         echo "No se encontró un archivo requirements.txt en el repositorio."
     fi
@@ -25,20 +28,20 @@ else
     REQ_FILE=$(find "$DIRECTORIO" -name "viz.py" 2>/dev/null | head -n 1)
     
     if [ -n "$REQ_FILE" ]; then
-        FRASE_ORIGINAL='def show(self, field="displacement", component=(0, 0), frame=0, quiverdisp=False, **kwargs):'
-        FRASE_NUEVA='def show(self, field="displacement", component=(0, 0), frame=0, quiverdisp=False, ruta_save = None, **kwargs):'
-
-        sed -i "s|$FRASE_ORIGINAL|$FRASE_NUEVA|g" $REQ_FILE
+        FRASE_ORIGINAL='def show(self, field="displacement", component=\(0, 0\), frame=0, quiverdisp=False, \*\*kwargs):'
+        FRASE_NUEVA='def show(self, field="displacement", component=(0, 0), frame=0, quiverdisp=False, ruta_save=None, **kwargs):'
+        
+        sed -i "s|$FRASE_ORIGINAL|$FRASE_NUEVA|g" "$REQ_FILE"
 
         FRASE_ORIGINAL='keyword = field.replace(" ", "").lower()'
         FRASE_NUEVA='keyword = field.replace(" ", "").lower()'$'\n''        plt.figure(figsize=(10, 10))'
 
-        sed -i "s|$FRASE_ORIGINAL|$FRASE_NUEVA|g" $REQ_FILE
+        sed -i "s|$FRASE_ORIGINAL|$FRASE_NUEVA|g" "$REQ_FILE"
         
         FRASE_ORIGINAL='plt.show()'
-        FRASE_NUEVA='if type(ruta_save) == str: plt.savefig(ruta_save, dpi=300)'
+        FRASE_NUEVA='if isinstance(ruta_save, str): plt.savefig(ruta_save, dpi=300)'
 
-        sed -i "s|$FRASE_ORIGINAL|$FRASE_NUEVA|g" $REQ_FILE
+        sed -i "s|$FRASE_ORIGINAL|$FRASE_NUEVA|g" "$REQ_FILE"
     fi
 fi
 
